1fa4be8335da942519d00a5202ea01a5f7282e38
Alumno Plaza Vespucio <pv-alumno@duoc.acad>
2026-09-03 21:29:35 -0400

    JAVA-009 - Agregar clasificacion con else if

--- src/Main.java
+++ src/Main.java
@@ -19,10 +19,12 @@ public class Main {
 
         Estudiante estudiante = new Estudiante(nombre, carrera, edad);
 
-        if (edad >= 18) {
-            System.out.println("Acceso autorizado.");
+        if (edad < 18) {
+            System.out.println("Estudiante menor de edad.");
+        } else if (edad < 25) {
+            System.out.println("Estudiante joven.");
         } else {
-            System.out.println("Acceso restringido.");
+            System.out.println("Estudiante adulto.");
         }
 
         estudiante.mostrarInformacion();
